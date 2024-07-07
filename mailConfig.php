060ce77e7ee856e8f205f4f32a40342c91183d0e
Melvin <melvinkwanys@gmail.com>
2024-07-07 19:43:52 +0800

    -

--- mailConfig.php
+++ mailConfig.php
@@ -2,7 +2,7 @@
 
 define('MAILHOST', "smtp.gmail.com");
 define('USERNAME', "melvinkwanys@gmail.com");
-define('PASWORD', "eobecpptcchokkll ");
+define('PASWORD', "eobecpptcchokkll");
 define('SEND_FROM', "melvinkwanys@gmail.com");
 define('SEND_FROM_NAME',"MINIMALIST LIBRARY");
 define('REPLY_TO', "melvinkwanys@gmail.com");
